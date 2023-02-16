de8c87a8267f15868b70f7cfb5df214650dea946
Baraka <mulandibaraka@gmail.com>
2023-02-16 04:51:00 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /*
- * main - printing a line of code using puts 
+ * main - printing a line of code using puts
  *
- * return:0
- */
+ * Return:0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
